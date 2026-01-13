44d20fc505959683cfcec5997967fdb5f87912f0
David Rey <tintoelculo@gmail.com>
2026-01-13 10:16:01 +0100

    chore: Initial commit

---
+++ src/main/java/com/horno/webHostingWeb/WebHostingWebApplication.java
@@ -0,0 +1,13 @@
+package com.horno.webHostingWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHostingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebHostingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horno/webHostingWeb/WebHostingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horno.webHostingWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHostingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
