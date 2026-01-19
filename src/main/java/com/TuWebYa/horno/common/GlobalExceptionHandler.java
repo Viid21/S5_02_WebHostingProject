8852fb93b4b26626dfe1ea39a1583e24aa792250
David Rey <tintoelculo@gmail.com>
2026-01-19 18:09:35 +0100

    feat: created GlobalExceptionHandler.

---
+++ src/main/java/com/TuWebYa/horno/common/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.TuWebYa.horno.common;
+
+import com.TuWebYa.horno.user.application.exception.UserNotFoundException;
+import com.TuWebYa.horno.user.domain.exception.InvalidEmailException;
+import com.TuWebYa.horno.user.domain.exception.InvalidNameException;
+import com.TuWebYa.horno.user.domain.exception.InvalidPasswordException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import reactor.core.publisher.Mono;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(InvalidEmailException.class)
+    public Mono<ResponseEntity<String>> handleInvalidEmail(InvalidEmailException ex) {
+        return Mono.just(ResponseEntity.badRequest().body(ex.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    public Mono<ResponseEntity<String>> handleInvalidPassword(InvalidPasswordException ex) {
+        return Mono.just(ResponseEntity.badRequest().body(ex.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidNameException.class)
+    public Mono<ResponseEntity<String>> handleInvalidName(InvalidNameException ex) {
+        return Mono.just(ResponseEntity.badRequest().body(ex.getMessage()));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public Mono<ResponseEntity<String>> handleUserNotFound(UserNotFoundException ex) {
+        return Mono.just(ResponseEntity.badRequest().body(ex.getMessage()));
+    }
+}
