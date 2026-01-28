bd1434b95bd9fcbbdbe787400a3590a7e8080556
David Rey <tintoelculo@gmail.com>
2026-01-28 13:19:10 +0100

    delete: removing postgres auto Configuration.

--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    role TEXT NOT NULL
-);
