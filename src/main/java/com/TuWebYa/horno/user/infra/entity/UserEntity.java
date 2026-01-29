72ee6663c77c8e9a3a5e50f86c06cb16bc9fad21
David Rey <tintoelculo@gmail.com>
2026-01-29 13:22:05 +0100

    refactor: creating user id in entity.

--- src/main/java/com/TuWebYa/horno/user/infra/entity/UserEntity.java
+++ src/main/java/com/TuWebYa/horno/user/infra/entity/UserEntity.java
@@ -1,10 +1,20 @@
 package com.TuWebYa.horno.user.infra.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.UUID;
 
 @Table("users")
-public record UserEntity(@Id UUID id, String name, String email, String password, String role) {
+@Data
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    private UUID id = UUID.randomUUID();
+    private String name;
+    private String email;
+    private String password;
+    private String role;
 }
--- src/main/java/com/TuWebYa/horno/user/infra/mapper/UserMapper.java
+++ src/main/java/com/TuWebYa/horno/user/infra/mapper/UserMapper.java
@@ -4,27 +4,37 @@ import com.TuWebYa.horno.user.domain.model.*;
 import com.TuWebYa.horno.user.infra.entity.UserEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class UserMapper {
-    public static UserEntity toEntity(User user){
-        return new UserEntity(
-                user.getId().value(),
-                user.getName().value(),
-                user.getEmail().value(),
-                user.getPassword().hashed(),
-                user.getRole().name()
-        );
+    public static UserEntity toEntity(User user) {
+        UserEntity entity = new UserEntity();
+
+        if (user.getId() != null) {
+            entity.setId(user.getId().value());
+        } else {
+            entity.setId(UUID.randomUUID());
+        }
+
+        entity.setEmail(user.getEmail().value());
+        entity.setPassword(user.getPassword().hashed());
+        entity.setRole(user.getRole().toString());
+        entity.setName(user.getName().value());
+
+        return entity;
     }
+
 
     public static User toDomain(UserEntity entity) {
         User user = new User(
-                UserEmail.of(entity.email()),
-                UserPassword.fromHashed(entity.password()),
-                UserRole.valueOf(entity.role())
+                UserEmail.of(entity.getEmail()),
+                UserPassword.fromHashed(entity.getPassword()),
+                UserRole.valueOf(entity.getRole())
         );
-        user.setId(UserId.from(String.valueOf(entity.id())));
-        user.setName(UserName.of(entity.name()));
-        user.setRole(UserRole.valueOf(entity.role()));
+        user.setId(UserId.from(String.valueOf(entity.getId())));
+        user.setName(UserName.of(entity.getName()));
+        user.setRole(UserRole.valueOf(entity.getRole()));
 
         return user;
     }
